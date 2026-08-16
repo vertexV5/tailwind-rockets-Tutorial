1fcc34a2f290171cf284e250c7aebe1eb38cc789
zero5 <73668722+zero057@users.noreply.github.com>
2026-08-16 10:19:26 +0100

    finished footer section and added mobile humburger menu

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 import "./styles.css";
+const hamburgerMenuButton = document.querySelector<HTMLButtonElement>("#humburger-button")!;
+
+const mobileMenu = document.querySelector("#mobile-menu")!;
+const toggleMenu = () => {
+  mobileMenu.classList.toggle("hidden");
+  
+  hamburgerMenuButton.innerHTML = mobileMenu.classList.contains('hidden') ? "&#9776;" : "X";
+};
+hamburgerMenuButton.addEventListener("click", toggleMenu);
+mobileMenu.addEventListener("click", toggleMenu);
